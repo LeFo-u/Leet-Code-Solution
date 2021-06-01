09ee3f849587407bf704dcef417db0b9a8ade435
Ashish Yadav <84495458+LeFo-u@users.noreply.github.com>
2021-06-01 23:15:03 +0530

    Create 20. Valid Parentheses.cpp

---
+++ Easy/Stack/20. Valid Parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> v;
+        for(auto var:s) {
+            if(var=='(' or var=='{' or var=='[') v.push(var);  
+            else {
+                if(v.empty() || (v.top()=='(' && var!=')') ||
+                (v.top()=='{' && var!='}') || (v.top()=='['
+                && var!=']')) return false;
+                v.pop(); } }
+        return v.empty(); }
+};
