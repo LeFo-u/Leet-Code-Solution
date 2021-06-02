ccbfb71dd72ae26f5c668c2581a642bd621c9f25
Ashish Yadav <84495458+LeFo-u@users.noreply.github.com>
2021-06-02 12:23:13 +0530

    Added another method of solving.

--- Linked List/Easy/876. Middle of the Linked List.cpp
+++ Linked List/Easy/876. Middle of the Linked List.cpp
@@ -1,3 +1,5 @@
+// Solution : 1 ->-> Counting the length of Linked List and then going to the middle of the Linked List .. Time : O(n) Space : O(n)..
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
@@ -19,3 +21,14 @@ public:
             b=head;
             return b; } }
 };
+
+// Solution : 2 ->-> Using slow and fast pointer.. Time : O(n) Space : O(1)..
+//  a : Slow pointer will take one step and fast pointer two steps.
+//  b : So, when fast pointer reaches to the end of Linked List slow pointer will be at middleNode
+
+ListNode* middleNode(ListNode* head) {
+       ListNode *slow = head, *fast = head;
+       while (fast && fast->next)
+           slow = slow->next, fast = fast->next->next;
+       return slow;
+   }
